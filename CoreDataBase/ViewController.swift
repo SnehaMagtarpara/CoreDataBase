6187d15e34b1da51232e7f2225a3368fcac32929
Sneha magtarpara <snehamagtarpara@gmail.com>
2023-04-06 05:45:34 +0530

    Initial Commit

---
+++ CoreDataBase/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  CoreDataBase
+//
+//  Created by R93 on 06/04/23.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var nametextField: UITextField!
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+    @IBAction func adddataButtonAction(_ sender: UIButton) {
+        if let x = idTextField.text,let y = Int(x),let a = numberTextField.text ,let b = Int(a)
+        {
+            addData(id: y, name: nametextField.text ??  "", number: b)
+            
+        }
+        
+    }
+    func addData(id:Int,name:String,number:Int)
+    {
+        guard let appDeleget = UIApplication.shared.delegate as? AppDelegate else { return }
+        let manageContex = appDeleget.persistentContainer.viewContext
+        let userEntity = NSEntityDescription.entity(forEntityName: "Student", in: manageContex)!
+        let user = NSManagedObject(entity: userEntity, insertInto: manageContex)
+        user.setValue(name, forKey: "name")
+        user.setValue(id, forKey: "id")
+        user.setValue(number, forKey: "number")
+        print(user)
+        
+        
+        
+    }
+    func getdata()
+    {
+        
+    }
+    func upatedata()
+    {
+        
+    }
+    func deleteData()
+    {
+        
+    }
+
+
+}
+
