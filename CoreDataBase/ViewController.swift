f00dd7ac44a934da59d3a0dde8c3c048c35d4a6f
Sneha magtarpara <snehamagtarpara@gmail.com>
2023-04-11 05:13:50 +0530

    getData

--- CoreDataBase/ViewController.swift
+++ CoreDataBase/ViewController.swift
@@ -16,16 +16,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var numberTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-       
     }
-    
     @IBAction func adddataButtonAction(_ sender: UIButton) {
         if let x = idTextField.text,let y = Int(x),let a = numberTextField.text ,let b = Int(a)
         {
             addData(id: y, name: nametextField.text ??  "", number: b)
-            
         }
-        
+    }
+    @IBAction func deleteDataButtonAction(_ sender: UIButton) {
+        if let x = idTextField.text,let y = Int(x)   
+        {
+            removeData(id: y)
+        }
     }
     func addData(id:Int,name:String,number:Int)
     {
@@ -37,23 +39,55 @@ class ViewController: UIViewController {
         user.setValue(id, forKey: "id")
         user.setValue(number, forKey: "number")
         print(user)
-        
-        
-        
     }
+    
+    @IBAction func getDataButtonAction(_ sender: Any) {
+        getdata()
+    }
+    
     func getdata()
     {
+        guard let appDeleget = UIApplication.shared.delegate as? AppDelegate else { return }
+        let manageContex = appDeleget.persistentContainer.viewContext
+        let fetchRequest = Student.fetchRequest()
+        do
+        {
+            let result = try manageContex.fetch(fetchRequest)
+            for data in result
+            {
+                print(data.id,data.name as! String,data.number)
+            }
         
+        }
+        catch{
+            print("Data Not Get")
+        }
     }
+        
+    
     func upatedata()
     {
-        
     }
-    func deleteData()
+        func removeData(id:Int)
     {
-        
+        guard let appDeleget = UIApplication.shared.delegate as? AppDelegate else { return }
+        let manageContex = appDeleget.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Student")
+        fetchRequest.predicate = NSPredicate(format: "id = %d", id)
+        do
+        {
+            let test = try manageContex.fetch(fetchRequest)
+            let objectToDelete = test[0] as! NSManagedObject
+           manageContex.delete(objectToDelete)
+            appDeleget.saveContext()
+            print("Delete Data")
+        }
+        catch
+        {
+            print("Error")
+        }
     }
-
-
 }
+
+
 
